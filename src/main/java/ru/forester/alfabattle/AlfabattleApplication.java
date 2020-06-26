30eb7ba08ae73a1cc8584f668127107b834d645a
FORESTER <forester2k12@bk.ru>
2020-06-26 22:31:54 +0300

    Init commit

---
+++ src/main/java/ru/forester/alfabattle/AlfabattleApplication.java
@@ -0,0 +1,13 @@
+package ru.forester.alfabattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfabattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfabattleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/forester/alfabattle/AlfabattleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.forester.alfabattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfabattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
